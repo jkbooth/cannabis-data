a6fd90a70c423f681ddda113a01b8e49bcef0258
Judith <judith.booth@msl.ubc.ca>
2014-10-09 08:55:42 -0700

    made a plot and a script to calculate kovat's I

---
+++ potter_chart.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(ggthemes)
+monoterps <- c(39.12, 55.46, 60.41, 65.42, 70.55)
+sesquiters <- c(60.88, 44.55, 39.59, 34.58, 29.45)
+#enter the data
+
+str(monoterps)
+
+chart <- data.frame(monoterpenes = monoterps,
+                    sesquiterpenes = sesquiters,
+                    week = 1:5)
+
+
+#turn it into a dataframe
+
+str(chart)
+
+ggplot(data = chart, aes(x = week, y = monoterpenes, group = 1)) + geom_line() + geom_point()
+
+chart2 <- data.frame(week = 1:5,
+                     percent = c(monoterps,sesquiters), 
+                     legend = c(rep("monoterpenes", 5), rep("sesquiterpenes", 5)))
+
+plot <- ggplot(data = chart2,
+               aes(x = week, y = percent, group = legend, color = legend, ymax = 100, )) + geom_line(size = 1.5) +
+  geom_point(size = 4)
+
+plot <- plot + ggtitle("Floral Terpene Content")
+plot + xlab("Weeks Post Flowering") + ylab("% of total terpenes") + theme_few() + scale_colour_gdocs()
+
+#exports the most recent plot to file:
+ggsave("potter_terpene_plot.png")
---
+++ retentionindex_calc.R
@@ -0,0 +1,38 @@
+##This script requires dplyr. Enter the retention times of your unknown, and the script will output the retention index
+
+suppressMessages(library(dplyr))
+
+
+#As a numeric vector, the retention times of your alkanes
+rettimes <- c(4.787, 6.269, 7.515, 8.599, 9.586, 
+              10.495, 11.344, 12.151, 12.909, 13.632,
+              14.318, 14.968, 15.595, 16.191, 16.769,
+              17.316, 17.852, 18.358, 18.894, 19.508)
+
+#currently set up for C9 to C28. If your data is different, enter the alkanes you have data for
+
+alkanes <- 9:28
+
+retention <- function(unknown) {
+  retdf <- cbind.data.frame(alkanes, rettimes)
+  sub1 <- retdf %>%
+    filter(rettimes < unknown)
+  N <- as.numeric(max(sub1$alkanes))
+  sub2 <- retdf %>%
+    filter(rettimes > unknown)
+  N1 <- as.numeric(min(sub2$alkanes))
+  
+  p1 <- as.numeric(grep(N, retdf$alkanes))
+  p2 <- as.numeric(grep(N1, retdf$alkanes))
+  
+  t1 <- as.numeric(retdf$rettimes[p1])
+  t2 <- as.numeric(retdf$rettimes[p2])
+  return(print(100 * (N + (N1 - N) * ((unknown - t1) / (t2 - t1)))))
+}
+
+#the function retention takes as an argument the retention time of your unknown, and returns the retention index
+retention(11.157)
+
+
+
+
